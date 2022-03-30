a72087f5f8bd75211014959784c60c72dd365feb
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2022-03-29 21:50:03 -0300

    refactor(TaskList): Add best practices

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,9 +14,7 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
-    if (!newTaskTitle) {
-      return;
-    }
+    if (!newTaskTitle) return;
 
     const newTask: Task = {
       id: Date.now(),
@@ -24,17 +22,20 @@ export function TaskList() {
       isComplete: false,
     };
 
-    setTasks([...tasks, newTask]);
+    setTasks((oldState) => [...oldState, newTask]);
+    setNewTaskTitle("");
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
-    const changeIsComplete: Task[] = tasks.map((task) => {
-      if (task.id === id) {
-        task.isComplete = !task.isComplete;
-      }
-      return task;
-    });
+    const changeIsComplete: Task[] = tasks.map((task: Task) =>
+      task.id === id
+        ? {
+            ...task,
+            isComplete: !task.isComplete,
+          }
+        : task
+    );
 
     setTasks([...changeIsComplete]);
   }
